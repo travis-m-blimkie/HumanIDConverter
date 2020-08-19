0a12a40dd1bd635b0c1e2dc20ffd2d56093d9d59
Travis M Blimkie <travis.m.blimkie@gmail.com>
2020-08-19 11:49:05 -0700

    Modified script to make it slightly faster. Tested and updated app data.

--- make_conversion_table.R
+++ make_conversion_table.R
@@ -1,30 +1,25 @@
 
 # Script to generate table used by the app
-suppressPackageStartupMessages({
-  library(biomaRt)
-  library(tidyverse)
-})
+suppressPackageStartupMessages(library(biomaRt))
 
 today <- gsub(Sys.Date(), pattern = "-", replacement = "")
 
 biomart_table <- getBM(
-  attributes = c("ensembl_gene_id", "hgnc_symbol", "entrezgene_id", "uniprot_gn_id"),
+  attributes = c("hgnc_symbol", "ensembl_gene_id", "entrezgene_id", "uniprot_gn_id"),
   mart = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 )
 
 biomart_table[biomart_table == ""] <- NA
 
-biomart_table <- biomart_table %>%
-  select(
-    "HGNC"    = hgnc_symbol,
-    "Ensembl" = ensembl_gene_id,
-    "Entrez"  = entrezgene_id,
-    "UniProt" = uniprot_gn_id
-  )
+biomart_table <- dplyr::select(
+  .data     = biomart_table,
+  "HGNC"    = hgnc_symbol,
+  "Ensembl" = ensembl_gene_id,
+  "Entrez"  = entrezgene_id,
+  "UniProt" = uniprot_gn_id
+)
 
 saveRDS(
   biomart_table,
-  paste0(
-    "app_data/shiny_biomart_table_", today, ".Rds"
-  )
+  paste0("app_data/shiny_biomart_table_", today, ".Rds")
 )
